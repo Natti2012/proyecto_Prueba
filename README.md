e838a787cba1ef90fa9a2c54fdd2aa9e5c9c794e
ValentinoMartz <valentinomartinez55@gmail.com>
2022-05-20 12:21:48 -0300

    Vale v2

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Hola desde el brunch prueba 1.
+# Hola desde el brunch prueba 2.
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save by Valentino from Rama del Vale
-          brunch
+          v2 brunch
         </p>
         <a
           className="App-link"
