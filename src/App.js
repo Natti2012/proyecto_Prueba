65170de84d94608986e377f3be04e28c16321c3b
sureste <edipean@hotmail.com>
2022-05-20 10:28:14 -0500

    MECAGO

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          AAAAA <code>src/App.js</code> and save to reload.
+          pastel <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
